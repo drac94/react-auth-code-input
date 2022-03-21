6868174821c55b5a828da8ea7ca0f6ef7bb89b9f
drac94 <info@luisguerrero.me>
2022-03-21 12:40:58 -0600

    update demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,21 @@
 import React, { useRef, useState } from 'react';
 
-import AuthCode, { AuthCodeHandle } from 'react-auth-code-input';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { base16AteliersulphurpoolLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import AuthCode, { AuthCodeRef } from 'react-auth-code-input';
 import './index.css';
 
 const App = () => {
-  const fInput = useRef<AuthCodeHandle>(null);
+  const AuthInputRef = useRef<AuthCodeRef>(null);
   const [result, setResult] = useState<string>('');
   const handleOnChange = (res: string) => {
     setResult(res);
   };
+
   return (
     <div className='main'>
       <h1>React Auth Code Input</h1>
-      <p>One-time password (OTP) React input component.</p>
+      <p>One-time password (OTP) React component.</p>
       <div className='badges'>
         <img
           alt=''
@@ -40,125 +43,53 @@ const App = () => {
           View documentation on GitHub
         </a>
       </p>
-      <h3>Modes:</h3>
-      <ul>
-        <li>Alpha.</li>
-        <li>Alphanumeric (default).</li>
-        <li>Numeric.</li>
-      </ul>
-      <h3>Supports:</h3>
-      <ul>
-        <li>Pasting.</li>
-        <li>Move back using the backspace key.</li>
-        <li>SMS.</li>
-      </ul>
-      <h2>Default</h2>
-      <AuthCode ref={fInput} onChange={handleOnChange} />
-      {result && <p>Result: {result}</p>}
-      <button onClick={() => fInput.current?.focus()}>Focus</button>
-      <code>
-        {`
-import React, { useState } from 'react'
-import AuthCode from 'react-auth-code-input'
+      <div>
+        <div>
+          <AuthCode
+            ref={AuthInputRef}
+            onChange={handleOnChange}
+            containerClassName='container'
+            inputClassName='input'
+          />
+
+          <p>Result: {result}</p>
+          <button onClick={() => AuthInputRef.current?.focus()}>Focus</button>
+          <button onClick={() => AuthInputRef.current?.clear()}>Clear</button>
+        </div>
+        <code>
+          <SyntaxHighlighter
+            language='jsx'
+            style={base16AteliersulphurpoolLight}
+          >
+            {`
+import React, { useRef, useState } from 'react';
+import AuthCode, { AuthCodeRef } from 'react-auth-code-input';
 
 const App = () => {
+  const AuthInputRef = useRef<AuthCodeRef>(null);
   const [result, setResult] = useState<string>('');
   const handleOnChange = (res: string) => {
     setResult(res);
   };
   return (
     <div>
-      <AuthCode onChange={handleOnChange} />
-      {result && <p>Result: {result}</p>}
+      <AuthCode
+        ref={AuthInputRef}
+        onChange={handleOnChange}
+        containerClassName='container'
+        inputClassName='input'
+      />
+
+      <p>Result: {result}</p>
+      <button onClick={() => AuthInputRef.current?.focus()}>Focus</button>
+      <button onClick={() => AuthInputRef.current?.clear()}>Clear</button>
     </div>
   );
-        `}
-      </code>
-      <h2>Custom Styles</h2>
-      <p>
-        Customize the look by passing CSS classes to the{' '}
-        <i>containerClassName</i> and <i>inputClassName</i> properties.
-      </p>
-      <AuthCode
-        allowedCharacters='numeric'
-        onChange={() => null}
-        length={5}
-        containerClassName='container'
-        inputClassName='input'
-      />
-      <p>index.tsx</p>
-      <code>
-        {`
-import React from 'react'
-import AuthCode from 'react-auth-code-input'
-
-const App = () => {
-  return (<AuthCode
-    allowedCharacters='numeric'
-    length={5}
-    containerClassName='container'
-    inputClassName='input'
-    inputType='number'
-    onChange={handleOnChange}
-  />)
 }
         `}
-      </code>
-      <p>styles.css</p>
-      <code>
-        {`
-.container {
-  padding: 16px;
-}
-
-.input {
-  width: 2ch;
-  padding: 8px;
-  border-radius: 8px;
-  font-size: 40px;
-  text-align: center;
-  margin-right: 12px;
-  border: 1px solid white;
-  text-transform: uppercase;
-}
-        `}
-      </code>
-      <h2>Passwords</h2>
-      <p>Protect your inputs.</p>
-      <AuthCode
-        onChange={() => null}
-        length={5}
-        isPassword
-        containerClassName='container'
-        inputClassName='input'
-      />
-      <p>index.tsx</p>
-      <code>
-        {`
-import React from 'react'
-import AuthCode from 'react-auth-code-input'
-
-const App = () => {
-  return (<AuthCode
-    length={5}
-    isPassword
-    containerClassName='container'
-    inputClassName='input'
-    onChange={handleOnChange}
-  />)
-}
-        `}
-      </code>
-      <a
-        className='github-fork-ribbon'
-        href='https://github.com/drac94/react-auth-code-input'
-        data-ribbon='Fork me on GitHub'
-        title='Fork me on GitHub'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Fork me on GitHub
-      </a>
+          </SyntaxHighlighter>
+        </code>
+      </div>
     </div>
   );
 };
